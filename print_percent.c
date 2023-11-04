c071646ae404166b920b7010aa6af73276c05537
Greentaylor27 <7196@holbertonstudents.com>
2023-11-04 09:04:38 -0700

    First push of Print Percent. Function file for print precent

---
+++ print_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_percent - prints %
+ *
+ * Return: 1
+ */
+int print_percent(va_list arg)
+{
+	char s;
+
+	s = va_arg(arg, int);
+	putchar((char)s);
+	return (1);
+}
