5feb0a4a7ad3b291209987c844963d4de50fcbfd
David Alsabrook <Dfalsabrook@gmail.com>
2023-11-04 11:13:22 -0500

    adding our tested printf function

--- _printf.c
+++ _printf.c
@@ -1,10 +1,41 @@
+#include "main.h"
 /**
  * printf - prints user input and variable depending on a format specifier
  * @format: used to identify which datatype is passed in
  *
  * Return: number of characters printed to standard output
  */
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
+	va_list args;
+	int count;
+	spec_t obj;
 
+	va_start(args, format);
+
+	count = 0;
+
+	if (format == NULL)
+	{
+		return(98);
+	}
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			obj.func = get_form(format);
+			obj.func(args);
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+
+	return (count);
 }
