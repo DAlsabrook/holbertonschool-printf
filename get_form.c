275047e0b8671fffe624530d7e9c9d47c7f6cf70
David Alsabrook <Dfalsabrook@gmail.com>
2023-11-04 11:27:26 -0500

    Changed file name and added function pointer function

---
+++ get_form.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * get_form - point to correct function to use
+ * @s: char passed
+ *
+ * Return: int
+ */
+int (*get_form(const char *s))(va_list)
+{
+	spec_t spec[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (spec[i].id != NULL)
+	{
+		if((spec[i].id[0]) == s[0])
+		{
+			return (spec[i].func);
+		}
+		i++;
+	}
+
+	return (0);
+}
