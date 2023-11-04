a102ec775867fdd18ce6d31d97db21ffe6e66c3e
Greentaylor27 <7196@holbertonstudents.com>
2023-11-04 08:56:51 -0700

    First push of header file this should contain the first set of prototypes for task 0

--- main.h
+++ main.h
@@ -1,6 +1,21 @@
 #ifndef PRINT_F
 #define PRINT_F
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+typedef struct spec
+{
+	char *id;
+	int (*func)(va_list);
+}
+spec_t;
 
 int _printf(const char *format, ...);
+int (*get_form(const char *s))(va_list);
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
 
 #endif
